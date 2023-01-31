3de83917956e771be36f2f34e2df190e6f6f2171
Juan Mera Menéndez <79599587+juanmera01@users.noreply.github.com>
2023-01-31 09:52:17 +0100

    upload order service

---
+++ aws-lambda-order/src/test/java/com/lambda/order/awslambdaorder/AwsLambdaOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambda.order.awslambdaorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsLambdaOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
